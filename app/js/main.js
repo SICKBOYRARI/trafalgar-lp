b34b1bb92743278d44a0ac7bf475ea20d55a5c8c
SICKBOYRARI <jediz0rrr@gmail.com>
2020-10-02 13:47:59 +0700

    Added @media + slick slider + hamburger navbar

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,26 @@
 $(function(){
+    $('.customer__content').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 700,
+                settings: {
+                    arrows: false
+                }
+            }
+            ]
+    });
 
-
-
-  
+    $('#sandwichmenu').on('click', function(e) {
+        e.preventDefault();
+        $('#sandwichmenu').toggleClass("active");
+        $('.js_menu').toggleClass("active");
+        $('.header__ul').toggleClass("header__ul-active");
+        $('.header__li a').toggleClass("a-active");
+        $('.header__li').toggleClass("li-active");
+        // $('body').toggleClass("body-block")
+    });
 });
